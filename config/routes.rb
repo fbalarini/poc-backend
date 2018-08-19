64eb0ad849671939077c4b196a3deed8016af0cc
Arturo Collazo <accg14@acer.guest.fing.edu.uy>
2018-08-19 19:38:11 -0300

    +routesAdded

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors do
+    post 'new_author'
+    get 'author_books'
+    end
+  resources :books do
+    post 'new_book'
+    get 'book_pages'
+    get 'book_publishDate'
+  end
 end
