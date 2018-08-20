fe15978908a4af2511d2cbf60cb97e33e121d83f
Arturo Collazo <accg14@acer.guest.fing.edu.uy>
2018-08-20 15:53:33 -0300

    little corrections in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :authors do
-    post 'new_author'
-    get 'author_books'
+    post 'new'
+    get 'books'
     post 'add_book'
     end
   resources :books do
-    post 'new_book'
+    post 'new'
     get 'book_pages'
     get 'book_publishDate'
   end
