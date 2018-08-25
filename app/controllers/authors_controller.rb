0f768df930423b74c5bd8a41c2dd257987a53030
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2018-08-25 19:09:50 -0300

    Authors controller finished

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,64 @@
 class AuthorsController < ApplicationController
+
+  def index
+    authors = Author.all
+    render json: authors
+  end
+
+  def create
+    author = Author.new(authors_params)
+    if author.save
+      render json: author
+    else
+      render json: author.errors, status: 400
+    end
+  end
+
+  def show
+    if author_by_id.nil?
+      render json: {status: "error", code: 3000, message: "Author doesnt exists"}
+    else
+      render json: author_by_id
+    end
+  end
+
+  def update
+    author = author_by_id
+    if author.nil?
+      render json: {status: "error", code: 3000, message: "Author doesnt exists"}
+    else
+      author.attributes = authors_params
+      if author.save
+        render json: author
+      else
+        render json: {status: "error", code: 3000, message: "Cannot update author"}
+      end
+    end
+  end
+
+  def destroy
+    author = author_by_id
+    if author.nil?
+      render json: {status: "error", code: 3000, message: "Author doesnt exists"}
+    else
+      if author.destroy
+        render json: {status: "OK", code: 200, message: "Author deleted"}
+      else
+        render json: {status: "error", code: 3000, message: "Cannot destroy author"}
+      end
+    end
+  end
+  private
+
+  def authors_params
+    params.require(:author).permit(:name,:country,:birthDate)
+  end
+
+  def author_by_id
+    if Author.exists?(params[:id])
+      @author_by_id ||= Author.find(params[:id])
+    else
+      nil
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_200007) do
+ActiveRecord::Schema.define(version: 2018_08_25_203320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2018_08_25_200007) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end
