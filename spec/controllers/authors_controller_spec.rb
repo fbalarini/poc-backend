d281649f9fc0e75edd3271917671221425f1023a
Gastón Abellá <gabella@localhost.localdomain>
2018-08-27 20:05:10 -0300

    Author controller test fixed

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -11,22 +11,19 @@ RSpec.describe AuthorsController, type: :controller do
     let(:author) { a = Author.create(name: 'Pepe', country: 'Uruguay')
                    a.books.create(name: 'Libro1', pages: 35)
                    a }
+    let(:serializer) { AuthorSerializer.new(author) }
+    let(:serialization) { ActiveModelSerializers::Adapter.create(serializer) }
+    let(:subject) { JSON.parse(serialization.to_json) }
 
     it "returns http success" do
       expect(response).to have_http_status(:success)
     end
 
-    it "response with JSON body containing expected Article attributes" do
+    it "response with JSON body containing expected Author attributes" do
       hash_body = nil
       expect { hash_body = JSON.parse(response.body).with_indifferent_access }.not_to raise_exception
       expect(hash_body.keys).to match_array(["id", "name", "country", "birthDate", "book_names"])
-      expect(hash_body).to match({
-        id: author.id,
-        name: 'Pepe', 
-        country: 'Uruguay', 
-        birthDate: nil,
-        book_names: ["Libro1"]
-      })
+      expect(hash_body).to match(subject)
     end
   end
 end
