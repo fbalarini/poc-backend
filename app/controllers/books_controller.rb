d69691dfcd72efe7cac4a0901507c8863a8a32ff
Gastón Abellá <gabella@localhost.localdomain>
2018-08-27 19:28:52 -0300

    Book controller refactorized following PR review

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,57 +1,47 @@
 class BooksController < ApplicationController
 
   def index
-    if author_by_id.nil?
-      render json: {status: "error", code: 3000, message: "Author doesnt exist"}
-    else
       render json: author_by_id.books
-    end
   end
 
   def create
-    if !author_by_id.nil?
-      book = Book.new(book_params)
-      book[:author_id] = params[:author_id]
-      if book.save
-        render json: book
-      else
-        render json: {status: "error", code: 3000, message: "Cannot create book"}
-      end
+    book = Book.new(book_params)
+    book[:author_id] = params[:author_id]
+    if book.save
+      render json: book
     else
-      render json: {status: "error", code: 3000, message: "Author doesnt exist"}
+      render json: book.errors, status: 400
     end
   end
 
   def show
-    if check_params
-      return
+    if book_belongs_to_author
+      render json: book_by_id
+    else
+      render json: {status: "error", code: 3000, message: "The book doesnt belong to that author"}
     end
-    render json: book_by_id
   end
 
   def update
-    if check_params
+    unless book_belongs_to_author
+      render json: {status: "error", code: 3000, message: "The book doesnt belong to that author"}
       return
     end
-    book = book_by_id
-    book.attributes = book_params
-    if book.save
-      render json: book
+    book_by_id.attributes = book_params
+    if book_by_id.save
+      render json: book_by_id
     else
-      render json: {status: "error", code: 3000, message: "Cannot update book"}
+      render json: book_by_id.errors, status: 400
     end
   end
 
   def destroy
-    if check_params
+    unless book_belongs_to_author
+      render json: {status: "error", code: 3000, message: "The book doesnt belong to that author"}
       return
     end
-    book = book_by_id
-    if book.destroy
-      render json: {status: "OK", code: 200, message: "Book deleted"}
-    else
-      render json: {status: "error", code: 3000, message: "Cannot destroy book"}
-    end
+    book_by_id.destroy
+    head :ok
   end
 
 
@@ -62,47 +52,19 @@ class BooksController < ApplicationController
   private
 
   def author_by_id
-    if Author.exists?(params[:author_id])
-      @author_by_id ||= Author.find(params[:author_id])
-    else
-      nil
-    end
+    @author_by_id ||= Author.find(params[:author_id])
   end
 
   def book_by_id
-    if Book.exists?(params[:id])
-      @book_by_id ||= Book.find(params[:id])
-    else
-      nil
-    end
+    @book_by_id ||= Book.find(params[:id])
   end
 
   def book_params
     params.require(:book).permit(:name,:pages)
   end
 
-  def author_exists
-    !author_by_id.nil?
-  end
-
-  def book_exists
-    !book_by_id.nil?
-  end
-
-  def check_params
-    if !author_exists
-      render json: {status: "error", code: 3000, message: "Author doesnt exist"}
-      return true
-    end
-    if !book_exists
-      render json: {status: "error", code: 3000, message: "Book doesnt exist"}
-      return true
-    end
-    if book_by_id.author != author_by_id
-      render json: {status: "error", code: 3000, message: "The book doesnt belong to that author"}
-      return true
-    end
-    false
+  def book_belongs_to_author
+    book_by_id.author == author_by_id
   end
 
 end
