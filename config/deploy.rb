3cfc648973dad2aa7fec70fda23f9e74ecc8c0e0
Federico Balarini <federico.balarini1995@gmail.com>
2018-08-27 17:00:08 -0300

    Remove capistrano config

--- config/deploy.rb
+++
@@ -1,8 +0,0 @@
-lock '3.11.0'
-
-set :application, 'poc-backend'
-set :repo_url, 'git@github.com:fbalarini/poc-backend.git'
-set :user, 'ubuntu'
-set :rbenv_ruby, '2.5.1'
-
-set :puma_bind, 'tcp://0.0.0.0:9292'
--- config/deploy/staging.rb
+++
@@ -1,3 +0,0 @@
-set :stage, :staging
-
-server '18.223.116.238', user: 'ubuntu', roles: %w{app web db}
