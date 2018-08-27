c796a70937e45ab80d462a0e749158a69e26f64c
Gastón Abellá <gabella@localhost.localdomain>
2018-08-27 19:03:18 -0300

    Other PR changes about author controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -15,37 +15,23 @@ class AuthorsController < ApplicationController
   end
 
   def show
-    if author_by_id.nil?
-      render json: {status: "error", code: 3000, message: "Author doesnt exists"}
-    else
-      render json: author_by_id
-    end
+    render json: author_by_id
   end
 
   def update
-    if author_by_id.nil?
-      render json: {status: "error", code: 3000, message: "Author doesnt exists"}
+    author_by_id.attributes = authors_params
+    if author_by_id.save
+      render json: author_by_id
     else
-      author_by_id.attributes = authors_params
-      if author_by_id.save
-        render json: author_by_id
-      else
-        render json: {status: "error", code: 3000, message: "Cannot update author"}
-      end
+      render json: author_by_id.errors, status: 400
     end
   end
 
   def destroy
-    if author_by_id.nil?
-      render json: {status: "error", code: 3000, message: "Author doesnt exists"}
-    else
-      if author_by_id.destroy
-        render json: {status: "OK", code: 200, message: "Author deleted"}
-      else
-        render json: {status: "error", code: 3000, message: "Cannot destroy author"}
-      end
-    end
+    author_by_id.destroy
+    head :ok
   end
+  
   private
 
   def authors_params
@@ -53,10 +39,6 @@ class AuthorsController < ApplicationController
   end
 
   def author_by_id
-    if Author.exists?(params[:id])
       @author_by_id ||= Author.find(params[:id])
-    else
-      nil
-    end
   end
 end
