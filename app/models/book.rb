dada0e88fc70f28b754b375f90ead260020aced3
Arturo Collazo <accg14@gmail.com>
2018-08-29 19:34:21 -0300

    At book creation, dates control are performed. Rspec are OK

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :author
-  validates :name, :author, presence: true
+  validates :name, :author, :publishDate, presence: true
+  validate :publish_date
+
+  private
+
+  def publish_date
+    if author.present? && author.birthDate > publishDate
+      errors.add(:model_years, 'Invalid publish date')
+    end
+  end
 end
