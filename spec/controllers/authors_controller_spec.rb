699c5ce312a6c65899fd2a5eada9d5f5cf5c6246
gabella <gabella@localhost.localdomain>
2018-09-02 17:47:36 -0300

    Fixing tests after PR comments

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -3,76 +3,75 @@ require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
 
-  before(:each) do
-    @authors_with_books = create_list(:author_with_books, 3)
-  end
+	let(:hash_body) { JSON.parse(response.body) }
 
-  describe "GET #show" do
-    before do
-      get :show, params: {id: @authors_with_books.first.id}
-    end
+	before(:each) do
+		@authors_with_books = create_list(:author_with_books, 3)
+	end
 
-    let(:subject) { JSON.parse(serialize_author(@authors_with_books.first).to_json) }
-    let(:hash_body) { JSON.parse(response.body) }
+	describe "GET #show" do
+		before do
+			get :show, params: {id: @authors_with_books.first.id}
+		end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+		let(:subject) { JSON.parse(serialize_author(@authors_with_books.first).to_json) }
 
-    it "response with JSON body containing expected Author attributes" do
-      expect(hash_body.keys).to match_array(["id", "name", "country", "birthDate", "book_names"])
-      expect(hash_body).to match(subject)
-    end
-  end
+		it "returns http success" do
+			expect(response).to have_http_status(:success)
+		end
 
-  describe "GET #index" do
-    before do
-      get :index
-    end
+		it "response with JSON body containing expected Author attributes" do
+			expect(hash_body.keys).to match_array(["id", "name", "country", "birthDate", "book_names"])
+			expect(hash_body).to match(subject)
+		end
+	end
 
-    let(:subject) { JSON.parse(serialize_authors(@authors_with_books).to_json) }
-    let(:hash_body) { JSON.parse(response.body) }
+	describe "GET #index" do
+		before do
+			get :index
+		end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+		let(:subject) { JSON.parse(serialize_authors(@authors_with_books).to_json) }
 
-    it "response with JSON body containing expected Authors" do
-      expect(hash_body).to match(subject)
-    end
-  end
+		it "returns http success" do
+			expect(response).to have_http_status(:success)
+		end
 
-  describe "POST #create" do
-    before do
-      @new_author = build(:author)
-      post :create, params: {author: @new_author.attributes}
-    end
+		it "response with JSON body containing expected Authors" do
+			expect(hash_body).to match(subject)
+		end
+	end
 
-    let(:subject) { JSON.parse(serialize_author(Author.find_by name: @new_author.name).to_json) }
-    let(:hash_body) { JSON.parse(response.body) }
-    
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+	describe "POST #create" do
+		before do
+			@new_author = build(:author)
+			post :create, params: {author: @new_author.attributes}
+		end
 
-    it "response with JSON body containing recently created Author" do
-      expect(hash_body).to match(subject)
-    end
-  end
+		let(:subject) { JSON.parse(serialize_author(Author.find_by name: @new_author.name).to_json) }
 
-  describe "DELETE #destroy" do
-    before do
-      @id_to_destroy = @authors_with_books.first.id
-      delete :destroy, params: {id: @id_to_destroy }
-    end
+		it "returns http success" do
+			expect(response).to have_http_status(:success)
+		end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+		it "response with JSON body containing recently created Author" do
+			expect(hash_body).to match(subject)
+		end
+	end
 
-    it "destroys de desired author" do
-      expect{ Author.find(@id_to_destroy) }.to raise_error(ActiveRecord::RecordNotFound)
-    end
+	describe "DELETE #destroy" do
+		before do
+			@id_to_destroy = @authors_with_books.first.id
+			delete :destroy, params: {id: @id_to_destroy }
+		end
 
-  end
+		it "returns http success" do
+			expect(response).to have_http_status(:success)
+		end
+
+		it "destroys the desired author" do
+			expect(Author.count).to eql 2
+		end
+
+	end
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
 
-  before(:each) do
-    #@author_list = create_list(:author, 3)
-    @authors_with_books = create_list(:author_with_books, 3)
-    @author = @authors_with_books.first
-    @book = @author.books.first
-  end
+	before(:each) do
+		#@author_list = create_list(:author, 3)
+		@authors_with_books = create_list(:author_with_books, 3)
+		@author = @authors_with_books.first
+		@book = @author.books.first
+	end
 
 	describe "GET #all_books" do
 		before do
@@ -27,72 +27,72 @@ RSpec.describe BooksController, type: :controller do
 		end
 	end
 
-  describe "GET #show" do
-    before do
-  		get :show, params: {author_id: @author.id, id: @book.id}
-    end
+	describe "GET #show" do
+		before do
+			get :show, params: {author_id: @author.id, id: @book.id}
+	end
 
-    let(:subject) { JSON.parse(serialize_book(@book).to_json) }
-    let(:hash_body) { JSON.parse(response.body) }
+		let(:subject) { JSON.parse(serialize_book(@book).to_json) }
+		let(:hash_body) { JSON.parse(response.body) }
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+		it "returns http success" do
+			expect(response).to have_http_status(:success)
+		end
 
-    it "response with JSON body containing expected Book attributes" do
-      expect(hash_body.keys).to match_array(["id", "author_name", "name", "pages"])
-      expect(hash_body).to match(subject)
-    end
+		it "response with JSON body containing expected Book attributes" do
+			expect(hash_body.keys).to match_array(["id", "author_name", "name", "pages"])
+			expect(hash_body).to match(subject)
+		end
 	end
 	
-  describe "GET #index" do
-    before do
-      get :index, params: {author_id: @author.id}
-    end
+	describe "GET #index" do
+		before do
+			get :index, params: {author_id: @author.id}
+		end
 
-    let(:subject) { JSON.parse(serialize_books(@author.books).to_json) }
-    let(:hash_body) { JSON.parse(response.body) }
+		let(:subject) { JSON.parse(serialize_books(@author.books).to_json) }
+		let(:hash_body) { JSON.parse(response.body) }
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+		it "returns http success" do
+			expect(response).to have_http_status(:success)
+		end
 
-    it "response with JSON body containing expected Books" do
-      expect(hash_body).to match(subject)
-    end
+		it "response with JSON body containing expected Books" do
+			expect(hash_body).to match(subject)
+		end
 	end
 
 	describe "POST #create" do
-    before do
-      @new_book = build(:book)
-      post :create, params: {author_id: @author.id, book: @new_book.attributes}
-    end
+		before do
+			@new_book = build(:book)
+			post :create, params: {author_id: @author.id, book: @new_book.attributes}
+		end
 
-    let(:subject) { JSON.parse(serialize_book(Book.find_by name: @new_book.name).to_json) }
-    let(:hash_body) { JSON.parse(response.body) }
-    
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+		let(:subject) { JSON.parse(serialize_book(Book.find_by name: @new_book.name).to_json) }
+		let(:hash_body) { JSON.parse(response.body) }
+		
+		it "returns http success" do
+			expect(response).to have_http_status(:success)
+		end
 
-    it "response with JSON body containing recently created Book" do
-      expect(hash_body).to match(subject)
-    end
+		it "response with JSON body containing recently created Book" do
+			expect(hash_body).to match(subject)
+		end
 	end
 	
-  describe "DELETE #destroy" do
+	describe "DELETE #destroy" do
 		before do
-      @id_to_destroy = @book.id
-      delete :destroy, params: {id: @id_to_destroy, author_id: @author.id }
-    end
+			@id_to_destroy = @book.id
+			delete :destroy, params: {id: @id_to_destroy, author_id: @author.id }
+		end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
-    end
+		it "returns http success" do
+			expect(response).to have_http_status(:success)
+		end
 
-    it "destroys de desired book" do
-      expect{ Book.find(@id_to_destroy) }.to raise_error(ActiveRecord::RecordNotFound)
-    end
+		it "destroys thee desired book" do
+			expect(Book.count).to eql 29
+		end
 
 	end
 	
